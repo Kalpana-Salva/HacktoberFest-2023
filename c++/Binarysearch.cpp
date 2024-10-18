a3a3209c3ade09edf22055595cc6073c0cea9107
Kalpana-Salva <153592016+Kalpana-Salva@users.noreply.github.com>
2024-10-18 19:49:33 +0530

    Update Binarysearch.cpp
    adding features such as input validation, handling edge cases, and providing a more user-friendly interface

--- c++/Binarysearch.cpp
+++ c++/Binarysearch.cpp
@@ -1,42 +1,38 @@
-#include <iostream>
-//time complexityy if O(log n)
-using namespace std;
-int binarysearch(int arr[] , int size , int key){
-    int start =0;
-    int end=size-1;
-    int mid=(start+end)/2;
-    //also;
-    //mid== start + (end-start)/2
-    while(start<=end){
-        if(arr[mid]==key){
-            return mid;
-        }
-        //right bala part par jayo 
-        if(key>arr[mid]){
-            //right part
-            start=mid+1;
-        }
-        //left bale part par jayo
-        else if(key<arr[mid]){
-            end=mid-1;
-        }
-        //mid ka value bhi update kar do
-        mid=(start+end)/2;
-    }
-    // if key is not present in the array the we return -1
-    
-    return -1;
-}
-int main()
-{
-    int even[]={2,4,6,8,12,18};
-    int odd[]={3,8,11,14,16};
-    
-    int index1=binarysearch(even , 6,12);
-    cout<<"index of the key in the array is  ="<<index1<<endl;
-    
-    int index2=binarysearch(odd , 5,10);
-    cout<<"index of the key in the array is  ="<<index2<<endl;
+'''def binarySearch(arr, l, r, x):
 
-    return 0;
-}
+	while l <= r:
+
+		mid = l + (r - l) // 2
+
+		# Check if x is present at mid
+		if arr[mid] == x:
+			return mid
+
+		# If x is greater, ignore left half
+		elif arr[mid] < x:
+			l = mid + 1
+
+		# If x is smaller, ignore right half
+		else:
+			r = mid - 1
+
+	# If we reach here, then the element
+	# was not present
+	return -1
+
+
+# Driver Code
+if name == 'main':
+	arr = [2, 3, 4, 10, 40]
+	x = 10
+
+	# Function call
+	result = binarySearch(arr, 0, len(arr)-1, x)
+	if result != -1:
+		print("Element is present at index", result)
+	else:
+		print("Element is not present in array")
+
+
+
+'''
