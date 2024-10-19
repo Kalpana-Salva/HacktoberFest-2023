a5b1543f4f83225a3c368260e2fd0cfe17ca7c82
Kalpana-Salva <153592016+Kalpana-Salva@users.noreply.github.com>
2024-10-19 09:15:34 +0530

    Update jump_game.c
    updating the jumo_game statement using c programming has cleared the error successfully

--- C/jump_game.c
+++ C/jump_game.c
@@ -1,36 +1,38 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+// Function to check if you can reach the last index
 bool canJump(int *nums, int numsSize)
 {
-    int maxReach = 0;
+    int maxReach = 0;  // Variable to store the maximum reachable index
 
     for (int i = 0; i < numsSize; i++)
     {
-        // If we've reached a position that is not reachable, return false
+        // If current index is beyond the maximum reachable index, return false
         if (i > maxReach)
         {
             return false;
         }
 
-        // Update the maximum reachable position
+        // Update the maximum reachable index
         maxReach = (i + nums[i] > maxReach) ? (i + nums[i]) : maxReach;
 
-        // If we can reach the end of the array, return true
+        // If we can reach or go beyond the last index, return true
         if (maxReach >= numsSize - 1)
         {
             return true;
         }
     }
 
-    return false;
+    return false;  // If we exit the loop and haven't returned true, we can't reach the end
 }
 
 int main()
 {
-    int nums[] = {2, 3, 1, 1, 4}; // Example input array
-    int numsSize = sizeof(nums) / sizeof(nums[0]);
+    int nums[] = {2, 3, 1, 1, 4};  // Example input array
+    int numsSize = sizeof(nums) / sizeof(nums[0]);  // Size of the array
 
+    // Check if it's possible to reach the last index
     if (canJump(nums, numsSize))
     {
         printf("Yes, you can reach the end of the array!\n");
