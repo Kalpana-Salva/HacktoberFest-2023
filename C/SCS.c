637dd806682bd54adfd09f67996ed6f47616b53a
Kalpana-Salva <153592016+Kalpana-Salva@users.noreply.github.com>
2024-10-19 09:00:54 +0530

    Update SCS.c

--- C/SCS.c
+++ C/SCS.c
@@ -1,4 +1,3 @@
-//Script to find the Shortest COmmon Subsequence
 #include <stdio.h>
 #include <string.h>
 
@@ -6,43 +5,45 @@ int max(int a, int b) {
     return (a > b) ? a : b;
 }
 
-int shortestCommonSupersequenceLength(char X[], char Y[]) {
+// Function to compute the length of the Shortest Common Supersequence (SCS)
+int shortestCommonSupersequenceLength(char X[], char Y[], int dp[][100]) {
     int m = strlen(X);
     int n = strlen(Y);
-
-    int dp[m + 1][n + 1];
 
     for (int i = 0; i <= m; i++) {
         for (int j = 0; j <= n; j++) {
             if (i == 0)
-                dp[i][j] = j;
+                dp[i][j] = j;  // If X is empty, add all characters from Y
             else if (j == 0)
-                dp[i][j] = i;
+                dp[i][j] = i;  // If Y is empty, add all characters from X
             else if (X[i - 1] == Y[j - 1])
-                dp[i][j] = 1 + dp[i - 1][j - 1];
+                dp[i][j] = 1 + dp[i - 1][j - 1];  // If last characters match
             else
-                dp[i][j] = 1 + max(dp[i - 1][j], dp[i][j - 1]);
+                dp[i][j] = 1 + max(dp[i - 1][j], dp[i][j - 1]);  // Else take the minimum
         }
     }
 
-    return dp[m][n];
+    return dp[m][n];  // The value at dp[m][n] gives the SCS length
 }
 
-void printSCS(char X[], char Y[]) {
+// Function to print the Shortest Common Supersequence (SCS)
+void printSCS(char X[], char Y[], int dp[][100]) {
     int m = strlen(X);
     int n = strlen(Y);
-    int SCSLength = shortestCommonSupersequenceLength(X, Y);
-    char SCS[SCSLength + 1];
+    int SCSLength = dp[m][n];  // We already calculated the SCS length in dp array
+    char SCS[SCSLength + 1];   // Array to store the SCS
+    SCS[SCSLength] = '\0';     // Null-terminate the string
 
     int i = m, j = n, k = SCSLength;
 
+    // Backtrack to find the SCS
     while (i > 0 && j > 0) {
-        if (X[i - 1] == Y[j - 1]) {
+        if (X[i - 1] == Y[j - 1]) {  // If characters match
             SCS[k - 1] = X[i - 1];
             i--;
             j--;
             k--;
-        } else if (dp[i - 1][j] > dp[i][j - 1]) {
+        } else if (dp[i - 1][j] > dp[i][j - 1]) {  // Move in the direction of greater value
             SCS[k - 1] = X[i - 1];
             i--;
             k--;
@@ -53,19 +54,19 @@ void printSCS(char X[], char Y[]) {
         }
     }
 
+    // Add remaining characters of X
     while (i > 0) {
         SCS[k - 1] = X[i - 1];
         i--;
         k--;
     }
 
+    // Add remaining characters of Y
     while (j > 0) {
         SCS[k - 1] = Y[j - 1];
         j--;
         k--;
     }
-
-    SCS[SCSLength] = '\0';
 
     printf("Shortest Common Supersequence: %s\n", SCS);
 }
@@ -74,10 +75,15 @@ int main() {
     char X[] = "ABCBDAB";
     char Y[] = "BDCABA";
 
-    int SCSLength = shortestCommonSupersequenceLength(X, Y);
+    // Create a DP array to store intermediate results
+    int dp[100][100];  // Assuming max length of strings is less than 100
+
+    // Get the length of the SCS
+    int SCSLength = shortestCommonSupersequenceLength(X, Y, dp);
     printf("Length of Shortest Common Supersequence: %d\n", SCSLength);
 
-    printSCS(X, Y);
+    // Print the SCS
+    printSCS(X, Y, dp);
 
     return 0;
 }
